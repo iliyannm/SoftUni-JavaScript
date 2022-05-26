b086f0ff99d424a33ec14d7617415720e5953177
iliyannm <ilian.maznikov@gmail.com>
2022-05-26 13:50:53 +0300

    update

--- Syntax, Functions and Statements/06. Road Radar.js
+++ Syntax, Functions and Statements/06. Road Radar.js
@@ -1,60 +1,43 @@
 function roadRadar(speed, area) {
-    if (area == 'residential') {
-        if (speed <= 20){
-            console.log(`Driving ${speed} km/h in a 20 zone`)
-        }
-        else if (speed <= 40){
-            console.log(`The speed is ${speed - 20} km/h faster than the allowed speed of 20 - speeding`)
-        }
-        else if (speed <= 60){
-            console.log(`The speed is ${speed - 20} km/h faster than the allowed speed of 20 - excessive speeding`)
-        }
-        else {
-            console.log(`The speed is ${speed - 20} km/h faster than the allowed speed of 20 - reckless driving`)
-        }
+    let result = '';
+    let speedLimit = 0;
+    let difference = 0;
+    let status = '';
+
+    switch(area){
+        case 'city':
+            speedLimit = 50;
+            break;
+        case 'residential':
+            speedLimit = 20;
+            break;
+        case 'interstate':
+            speedLimit = 90;
+            break;
+        case 'motorway':
+            speedLimit = 130;
+            break;
+        default:
+            break;
     }
-    else if (area == 'city') {
-        if (speed <= 50){
-            console.log(`Driving ${speed} km/h in a 50 zone`)
+
+    if (speed <= speedLimit && speed > 0) {
+        result = `Driving ${speed} km/h in a ${speedLimit} zone`
+    } else {
+        difference = speed - speedLimit;
+
+        if (difference <= 20) {
+            status = 'speeding';
+        } else if (difference <= 40) {
+            status = 'excessive speeding';
+        } else {
+            status = 'reckless driving';
         }
-        else if (speed <= 70){
-            console.log(`The speed is ${speed - 50} km/h faster than the allowed speed of 50 - speeding`)
-        }
-        else if (speed <= 90){
-            console.log(`The speed is ${speed - 50} km/h faster than the allowed speed of 50 - excessive speeding`)
-        }
-        else {
-            console.log(`The speed is ${speed - 50} km/h faster than the allowed speed of 50 - reckless driving`)
-        }
+
+        result = `The speed is ${difference} km/h faster than the allowed speed of ${speedLimit} - ${status}`
     }
-    else if (area == 'interstate') {
-        if (speed <= 90){
-            console.log(`Driving ${speed} km/h in a 90 zone`)
-        }
-        else if (speed <= 110){
-            console.log(`The speed is ${speed - 90} km/h faster than the allowed speed of 90 - speeding`)
-        }
-        else if (speed <= 130){
-            console.log(`The speed is ${speed - 90} km/h faster than the allowed speed of 90 - excessive speeding`)
-        }
-        else {
-            console.log(`The speed is ${speed - 90} km/h faster than the allowed speed of 90 - reckless driving`)
-        }
-    }
-    else if (area == 'motorway') {
-        if (speed <= 130){
-            console.log(`Driving ${speed} km/h in a 130 zone`)
-        }
-        else if (speed <= 150){
-            console.log(`The speed is ${speed - 130} km/h faster than the allowed speed of 130 - speeding`)
-        }
-        else if (speed <= 170){
-            console.log(`The speed is ${speed - 130} km/h faster than the allowed speed of 130 - excessive speeding`)
-        }
-        else {
-            console.log(`The speed is ${speed - 130} km/h faster than the allowed speed of 130 - reckless driving`)
-        }
-    }
+
+    console.log(result)
 }
 
 roadRadar(40, 'city')
