eecab0388e619487ecbfd5a4fad2c6dd3a62113e
iliyannm <ilian.maznikov@gmail.com>
2022-06-13 23:47:53 +0300

    objects

---
+++ Objects and Composition/01. City Record.js
@@ -0,0 +1,14 @@
+function solve(name, population, treasury) {
+    const result = {
+        name,
+        population,
+        treasury
+    };
+
+    return result;
+}
+
+
+console.log(solve('Tortuga',
+7000,
+15000))
---
+++ Objects and Composition/02. Town Population.js
@@ -0,0 +1,23 @@
+function solve(arr) {
+    const dict = {};
+
+    for (let el of arr) {
+        [city, population] = el.split(' <-> ');
+
+        if (dict[city] == undefined) {
+            dict[city] = 0
+        };
+
+        dict[city] += Number(population);
+    }
+
+    for (c in dict) {
+        console.log(`${c} : ${dict[c]}`)
+    }
+}
+
+solve(['Sofia <-> 1200000',
+'Montana <-> 20000',
+'New York <-> 10000000',
+'Washington <-> 2345000',
+'Las Vegas <-> 1000000'])
