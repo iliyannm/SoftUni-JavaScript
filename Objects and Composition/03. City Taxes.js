15300a593ee722bfef2831adf80e43033767de9c
iliyannm <ilian.maznikov@gmail.com>
2022-06-14 12:26:17 +0300

    objects

---
+++ Objects and Composition/03. City Taxes.js
@@ -0,0 +1,19 @@
+function solve(name, population, treasury) {
+    const result = {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasury += Math.floor(this.population * this.taxRate)
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor(this.population * percentage / 100)
+        },
+        applyRecession(percentage) {
+            this.treasury -= Math.floor(this.treasury * percentage / 100)
+        }
+    };
+
+    return result;
+}
---
+++ Objects and Composition/04. Object Factory.js
@@ -0,0 +1,49 @@
+function factory(lib, arr) {
+    let finalArray = [];
+
+    for (let order of arr) {
+        let currentObject = {'name': order.template.name};
+        for (part of order.parts) {
+            currentObject[part] = lib[part];
+        };
+
+        finalArray.push(currentObject);
+    };
+
+    return finalArray;
+};
+
+
+const library = {
+    print: function () {
+        console.log(`${this.name} is printing a page`);
+    },
+    scan: function () {
+        console.log(`${this.name} is scanning a document`);
+    },
+    play: function (artist, track) {
+        console.log(`${this.name} is playing '${track}' by ${artist}`);
+    },
+};
+
+const orders = [
+    {
+        template: { name: 'ACME Printer' },
+        parts: ['print']
+    },
+    {
+        template: { name: 'Initech Scanner' },
+        parts: ['scan']
+    },
+    {
+        template: { name: 'ComTron Copier' },
+        parts: ['scan', 'print']
+    },
+    {
+        template: { name: 'BoomBox Stereo' },
+        parts: ['play']
+    }
+];
+
+const products = factory(library, orders);
+console.log(products);
