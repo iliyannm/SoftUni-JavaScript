eaf7c5887bef39f5951a4c5c71a756e13a516d28
iliyannm <ilian.maznikov@gmail.com>
2022-10-07 08:54:11 +0300

    DOM

--- DOM Introduction/05. Table - Search Engine/solution.js
+++ DOM Introduction/05. Table - Search Engine/solution.js
@@ -1,10 +1,11 @@
-function solve() {
+function solve () {
+   let collection = document.getElementsByTagName('tbody tr');
+   let keyWord = document.getElementById('searchField');
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-   let listElements = document.querySelectorAll('tbody tr')
-   let keyWord = document.getElementById('searchField')
+
 
    function onClick() {
-      for (let el of listElements) {
+      for (let el of collection) {
          el.classList.remove('select')
          if (el.innerHTML.includes(keyWord.value)) {
             el.className = 'select'
