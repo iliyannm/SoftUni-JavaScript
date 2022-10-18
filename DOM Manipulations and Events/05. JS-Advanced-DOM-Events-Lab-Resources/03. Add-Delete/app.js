ea6725822d35c5b5337a9d2516093537418b9a2b
iliyannm <ilian.maznikov@gmail.com>
2022-10-18 22:29:15 +0300

    DOM Events

--- DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
+++ DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    let input = document.getElementById('newItemText');
+    let newLi = document.createElement('li');
+    newLi.textContent = input.value;
+
+    let deleteBtn = document.createElement('a');
+    deleteBtn.textContent = '[Delete]';
+    deleteBtn.href = '#';
+    newLi.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener('click', onDelete)
+
+    document.getElementById('items').appendChild(newLi);
+    document.getElementById('newItemText').value = '';
+
+    function onDelete(event) {
+        event.target.parentElement.remove();
+    }
 }
--- DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
+++ DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
@@ -1,3 +1,9 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', mouseMove);
+    const result = document.getElementById('result');
+
+    function mouseMove(event) {
+        result.textContent = Math.floor(event.offsetX / gradient.clientWidth * 100) + '%';
+    }
 }
