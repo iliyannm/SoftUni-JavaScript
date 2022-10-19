6865a0915b1ae6ac81c147b2d0a02b087470e308
iliyannm <ilian.maznikov@gmail.com>
2022-10-20 00:15:55 +0300

    DOM Events

--- DOM Manipulations and Events/01. Sections/script.js
+++ DOM Manipulations and Events/01. Sections/script.js
@@ -1,3 +1,25 @@
 function create(words) {
-   console.log('TODO:...');
+	let content = document.getElementById('content');
+	let listOfDivs = [];
+
+	for (let word of words) {
+		let div = document.createElement('div');
+		let p = document.createElement('p');
+		p.style.display = 'none';
+		p.textContent = word;
+		div.appendChild(p);
+		listOfDivs.push(div);
+	}
+
+	for (d of listOfDivs) {
+		content.appendChild(d);
+	}
+
+	content.addEventListener('click', showParagraph);
+
+	function showParagraph(event) {
+		if (event.target.tagName = 'DIV') {
+			event.target.children[0].style.display = '';
+		}
+	}
 }
--- DOM Manipulations and Events/02. Time-Converter/solution.js
+++ DOM Manipulations and Events/02. Time-Converter/solution.js
@@ -1,4 +1,31 @@
 function attachEventsListeners() {
+    let main = document.querySelector('main');
+    let elements = document.querySelectorAll('div input');
+    let input = 0;
+    main.addEventListener('click', convertData);
 
-    console.log('TODO:...');
+
+    function convertData(event) {
+        if (event.target.id == 'daysBtn') {
+            input = Number(elements[0].value);
+            elements[2].value = input * 24;
+            elements[4].value = input * 24 * 60;
+            elements[6].value = input * 24 * 60 * 60;
+        } else if (event.target.id == 'hoursBtn') {
+            input = elements[2].value;
+            elements[0].value = input / 24;
+            elements[4].value = input * 60;
+            elements[6].value = input * 60 * 60;
+        } else if (event.target.id == 'minutesBtn') {
+            input = elements[4].value;
+            elements[0].value = input / 60 / 24;
+            elements[2].value = input / 60;
+            elements[6].value = input * 60;
+        } else if (event.target.id == 'secondsBtn') {
+            input = elements[6].value;
+            elements[0].value = input / 60 / 60 / 24;
+            elements[2].value = input / 60 / 60;
+            elements[4].value = input / 60;
+        }
+    }
 }
