4700b64359d61f1f51b6c45478e7fc333a68040b
iliyannm <ilian.maznikov@gmail.com>
2022-10-19 09:41:30 +0300

    DOM Events

--- DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
+++ DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/03. Add-Delete/app.js
@@ -1,19 +1,23 @@
 function addItem() {
+    let list = document.getElementById('items');
     let input = document.getElementById('newItemText');
     let newLi = document.createElement('li');
     newLi.textContent = input.value;
+    list.addEventListener('click', onDelete)
 
     let deleteBtn = document.createElement('a');
     deleteBtn.textContent = '[Delete]';
     deleteBtn.href = '#';
     newLi.appendChild(deleteBtn);
 
-    deleteBtn.addEventListener('click', onDelete)
+    //deleteBtn.addEventListener('click', onDelete)
 
     document.getElementById('items').appendChild(newLi);
     document.getElementById('newItemText').value = '';
 
     function onDelete(event) {
-        event.target.parentElement.remove();
+        if (event.target.tagName = 'A') {
+            event.target.parentElement.remove();
+        }
     }
 }
--- DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
+++ DOM Manipulations and Events/05. JS-Advanced-DOM-Events-Lab-Resources/07. Shopping-Cart/solution.js
@@ -1,3 +1,48 @@
 function solve() {
-   //TODO...
+   let elements = [];
+   let price = 0;
+   let descriptionField = document.getElementsByTagName('textarea')[0];
+   let parentItemElement = ''
+   let bougthItem = ''
+   let money = 0;
+   let checkoutBtn = document.querySelectorAll('button')[3]
+   let existing = false;
+   let addButtons = document.querySelectorAll('div .product .product-add .add-product')
+
+   document.querySelector('div').addEventListener('click', addItem);
+   checkoutBtn.addEventListener('click', finishOrder)
+
+   function findEl(item, elems) {
+      for (el of elems) {
+         if (el == item) {
+            existing = true;
+         }
+      }
+      if (existing == false) {
+         elems.push(item)
+      } else {
+         existing = false
+      }
+   }
+
+   function addItem(event) {
+      if (event.target.tagName == 'BUTTON') {
+         parentItemElement = event.target.parentElement.parentElement;
+         bougthItem = parentItemElement.children[1].children[0].textContent;
+         findEl(bougthItem, elements);
+         money = parentItemElement.children[3].textContent;
+         price += Number(money);
+         descriptionField.disabled = false;
+         descriptionField.value += `Added ${bougthItem} for ${money} to the cart.` + '\n';
+         descriptionField.disabled = true;
+      }
+   }
+
+   function finishOrder(event) {
+      descriptionField.disabled = false;
+      descriptionField.value += `You bought ${Array.from(elements).join(', ')} for ${price.toFixed(2)}.` + '\n';
+      descriptionField.disabled = true;
+      document.querySelector('div').removeEventListener('click', addItem);
+      checkoutBtn.removeEventListener('click', finishOrder)
+   }
 }
