2f551d70575e3bf87731ef30721e387dad7a769e
iliyannm <ilian.maznikov@gmail.com>
2022-10-24 23:38:28 +0300

    DOM Events

--- DOM Manipulations and Events/03. Locked-Profile/app.js
+++ DOM Manipulations and Events/03. Locked-Profile/app.js
@@ -1,3 +1,29 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let main = document.querySelector('main');
+    main.addEventListener('click', showHideInfo);
+    main.addEventListener('click', lockUnlock);
+
+    function showHideInfo(event) {
+        if (event.target.tagName == 'BUTTON') {
+            if (event.target.textContent == 'Show more' && event.target.parentElement.children[4].checked == true) {
+                event.target.parentElement.children[9].style.display = 'block';
+                event.target.textContent = 'Hide it';
+            } else if (event.target.textContent == 'Hide it' && event.target.parentElement.children[4].checked == true) {
+                event.target.parentElement.children[9].style.display = 'none';
+                event.target.textContent = 'Show more';
+            }
+        }
+    }
+
+    function lockUnlock(event) {
+        if (event.target.type == 'radio') {
+            if (event.target.value == 'lock') {
+                event.target.checked = 'true';
+                event.target.parentElement.children[4].checked = '';
+            } else if (event.target.value == 'unlock') {
+                event.target.checked = 'true';
+                event.target.parentElement.children[2].checked = '';
+            }
+        }
+    }
 }
--- DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
+++ DOM Manipulations and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,13 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemText = document.getElementById('newItemText');
+    let newItemValue = document.getElementById('newItemValue');
+    let select = document.getElementById('menu');
+
+    let optionElement = document.createElement('option');
+    optionElement.textContent = newItemText.value;
+    optionElement.value = newItemValue.value;
+
+    select.appendChild(optionElement)
+    newItemText.value = '';
+    newItemValue.value = '';
 }
